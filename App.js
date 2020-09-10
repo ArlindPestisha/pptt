f4c82c63a3805c3113624bb29883f7f284e492e9
Arlind Pestisha <arlind>
2020-09-10 16:20:31 +0200

    fixing the app component for routes and navigation

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { LoginScreen, HomeScreen, RegistrationScreen} from './src/screens';
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import {decode, encode} from 'base-64'
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
 
-export default function App() {
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  
+  //Using Hooks for state
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+  
   return (
-    <View style={styles.container}>
-      <Text>open somtingh</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        //Here I am using the ternary operator ?  : ;
+        { user ? (
+          <Stack.Screen name='Home'>
+            {props => <HomeScreen {...props } extraData={user} />}
+          </Stack.Screen>
+        ) : (
+          <>
+            <Stack.Screen name='Login' component={LoginScreen} />
+            <Stack.Screen name='Registration' component={RegistrationScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
