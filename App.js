3f1c083e13cf50754cc10eba46e463f167a3b61a
Arlind Pestisha <arlind>
2020-09-14 15:31:05 +0200

    update in app.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { LoginScreen, HomeScreen, RegistrationScreen} from './src/screens';
 import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { firebase } from './src/firebase/config'
 import {decode, encode} from 'base-64'
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
@@ -15,6 +16,33 @@ const App = () => {
   //Using Hooks for state
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data()
+            setLoading(false)
+            setUser(userData)
+          })
+          .catch((error) => {
+            setLoading(false)
+          });
+      } else {
+        setLoading(false)
+      }
+    });
+  }, []);
+
+  if (loading) {
+    return (
+      <></>
+    )
+  }
   
   return (
     <NavigationContainer>
